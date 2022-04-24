1d70279c509db30ace54dec2e258eacecfbe3b14
Camille CAMBIUM <ccambium@student.42.fr>
2022-04-24 15:43:55 +0200

    fixed script

--- createDatabase.sql
+++ createDatabase.sql
@@ -17,12 +17,12 @@ CREATE TABLE IF NOT EXISTS comments (
 	username TEXT,
 	comment TEXT,
 	date DATE,
-	project_id NUMBER
+	project_id INT
 );
 
-INSERT INTO users (username, password) VALUES (admin, 8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918);
+INSERT INTO users (username, password) VALUES ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
 
 INSERT INTO projects (title, description, short_desc, image, groups) VALUES  ('Default', 'SomeDefaultDescription', 'SomeDefaultDescription', 'https://choualbox.com/Img/1611426483399.jpg', 'Default');
 
-INSERT INTO comment (username, comment, date ,project_id) VALUES ('defaultCommentator', 'An example of a comment', '2022-04-23', 1);
+INSERT INTO comments (username, comment, date ,project_id) VALUES ('defaultCommentator', 'An example of a comment', '2022-04-23', 1);
 
