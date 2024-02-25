83e000d70fd928b5265b3c26896ba317160ada54
Camille CAMBIUM <ccambium@student.42.fr>
2024-02-25 18:33:18 +0100

    UPDATE : app/api/projects
    changing response form 'projects route' to a json string with all projects in db

--- srcs/app/src/app/api/projects/route.ts
+++ srcs/app/src/app/api/projects/route.ts
@@ -1,3 +1,9 @@
-export function GET() : Response {
-	return new Response('projects route');
+import {PrismaClient} from '@prisma/client'
+
+
+const prisma = new PrismaClient();
+
+export async function GET() : Promise<Response> {
+	const allProjects = await prisma.projects.findMany();
+	return new Response(JSON.stringify(allProjects));
 }
