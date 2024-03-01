c6cc369af19190d545b0b1e14b8efa7e58dbfbd8
Camille CAMBIUM <royalfr@protonmail.com>
2024-03-01 11:30:16 +0100

    UPDATE : api/projects/[id]/route.ts
    updated route to return the projects asked by the id

--- srcs/app/src/app/api/projects/[id]/route.ts
+++ srcs/app/src/app/api/projects/[id]/route.ts
@@ -1,3 +1,17 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
 export async function GET ( request : Request, { params } : { params : {id : number} } ) : Promise<Response> {
-		return new Response(JSON.stringify(+params.id));
+
+	const project = await prisma.projects.findUnique({ 
+		where: {
+			id : +params.id,
+		}
+	});
+
+	if (project === null)
+		return new Response('Not Found', {status: 404});
+
+	return new Response(JSON.stringify(project));
 }
