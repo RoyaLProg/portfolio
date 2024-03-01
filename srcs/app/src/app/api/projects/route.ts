53a3124adea6602350d4116548b36f3e790db799
Camille CAMBIUM <royalfr@protonmail.com>
2024-03-01 11:31:01 +0100

    UPDATE : api/projects/route.ts
    Updated route to return the minimal information needed

--- srcs/app/src/app/api/projects/route.ts
+++ srcs/app/src/app/api/projects/route.ts
@@ -3,6 +3,13 @@ import {PrismaClient} from '@prisma/client'
 const prisma = new PrismaClient();
 
 export async function GET() : Promise<Response> {
-	const allProjects = await prisma.projects.findMany();
+	const allProjects = await prisma.projects.findMany({
+													  select: {
+														  id: true,
+														  title: true,
+														  image: true,
+														  short_desc: true
+													  }
+	});
 	return new Response(JSON.stringify(allProjects));
 }
