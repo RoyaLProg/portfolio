92f7fb795e876317ec64a41cc12ca12ace33af3d
Camille CAMBIUM <ccambium@student.42.fr>
2024-03-02 23:35:02 +0100

    UPDATE : api/users
    simple logic of connection returning a JWT for future identification purposes

--- srcs/app/src/app/api/users/route.ts
+++ srcs/app/src/app/api/users/route.ts
@@ -1,3 +1,32 @@
-export async function POST ( request : Request ) : Promise<Response> {
-	return new Response('OK');
+import jwt from 'jsonwebtoken'
+import { PrismaClient } from '@prisma/client'
+import { NextResponse } from 'next/server'
+
+const prisma = new PrismaClient();
+
+export async function POST ( request : Request ) : Promise<NextResponse> {
+	const body: Object = await request.json();
+	
+	if (!body || !body['username'] || !body['password'])
+		return new NextResponse('Error missing a field', {status: 400});
+
+	const count: number = await prisma.users.count({
+		where: {
+			username: body['username'],
+			password: body['password']
+		}
+	});
+
+	if (count !== 1)
+		return new NextResponse('Wrong credentials', {status: 401});
+	const secret = process.env.JWT_SECRET;
+	if (!secret)
+		return new NextResponse('Server Error', {status: 500});
+
+	const token = jwt.sign({username: body['username']}, secret)
+	const response = new NextResponse('OK');
+
+	response.cookies.set('identity', token);
+	return response;
 }
+
