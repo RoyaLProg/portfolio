f261a302e3c8fef630d1592276adbf116d70f3f4
Camille CAMBIUM <ccambium@student.42.fr>
2024-03-03 18:07:03 +0100

    UPDATE : api/projects
    Making the PATCH route so projects are now update-able

--- srcs/app/src/app/api/projects/[id]/route.ts
+++ srcs/app/src/app/api/projects/[id]/route.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { NextRequest } from 'next/server'
+import jwt from 'jsonwebtoken'
 
 const prisma = new PrismaClient();
 
@@ -16,6 +18,36 @@ export async function GET ( request : Request, { params } : { params : {id : num
 	return new Response(JSON.stringify(project));
 }
 
-export async function PATCH ( request : Request, { params } : { params : {id : number} } ) : Promise<Response> {
-	return new Response('OK');
+export async function PATCH ( request : NextRequest, { params } : { params : {id : number} } ) : Promise<Response> {
+	const newProject = request.json();
+	
+	const token =  request.cookies.get('identity');
+	const secret = process.env.JWT_SECRET;
+
+	if (!token || !token.value)
+		return new Response('Not identified', {status: 401});
+	if (!secret)
+		return new Response('Silly dev forgot to set JWT_SECRET environment variable');
+
+	try {
+		jwt.verify(JSON.parse(token.value), secret);
+	} catch {
+		return new Response('Token is not valid', {status: 401});
+	}
+
+	if (!newProject)
+		return new Response('No project given', {status: 400});
+
+	try {
+		await prisma.projects.update({
+			where: {
+				id: +params.id
+			},
+			data : newProject,
+		});
+	} catch {
+		return  new Response('Something went terribly wrong ...', {status: 500});
+	}
+
+	return new Response('Updated');
 }
