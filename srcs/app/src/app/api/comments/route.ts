5b4ef6e52f935f20b198780e475fa93ed69da671
Camille CAMBIUM <ccambium@student.42.fr>
2024-03-04 16:52:36 +0100

    UPDATE : api/comments
    adding POST route to add comment

--- srcs/app/src/app/api/comments/route.ts
+++ srcs/app/src/app/api/comments/route.ts
@@ -7,3 +7,33 @@ export async function GET ( ) : Promise<Response> {
 
 	return new Response(JSON.stringify(comments));
 }
+
+export async function POST ( request : Request ) : Promise<Response> {
+	const body = await request.json();
+
+	if (!body || !body['username'] || !body['comment'] || !body['project_id'])
+		return new Response('Error missing a field', {status: 400});
+
+	const count = await prisma.projects.count({
+		where: {
+			id: body['project_id']
+		}
+	});
+
+	if (count !== 1)
+		return new Response('Project does not exist', {status: 400});
+	const date = new Date();
+
+	console.log(date.toISOString().split('T')[0]);
+
+	prisma.comments.create({
+		data : {
+			username: body['username'],
+			comment: body['comment'],
+			project_id: body['project_id'],
+			date: date.toISOString().split('T')[0]
+		}
+	});
+
+	return new Response('OK');
+}
