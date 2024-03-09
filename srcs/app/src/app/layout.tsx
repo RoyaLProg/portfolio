129a3fa781efffa894d9b9ccb33640c5176e73cd
Camille CAMBIUM <ccambium@student.42.fr>
2024-03-09 17:55:04 +0100

    UPDATE : app/layout.tsx app/globals.css
    Adding font and changing body overallheight to fit all the pages (done and future ones)

--- srcs/app/src/app/layout.tsx
+++ srcs/app/src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 			<link rel="preconnect" href="https://fonts.googleapis.com"/>
 			<link rel="preconnect" href="https://fonts.gstatic.com"/>
 			<link href="https://fonts.googleapis.com/css2?family=Gantari:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+			<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"/>
 		</head>
 		<body className={inter.className}>{children}</body>
     </html>
