92fc7976b50a69e054e1d7c88987d90133508193
Camille CAMBIUM <royalfr@protonmail.com>
2024-03-13 10:07:06 +0100

    UPDATE : app/AboutMe.tsx
    Changing placeholders to actual image and text
    + changing next text to the next section title

--- srcs/app/src/app/AboutMe.tsx
+++ srcs/app/src/app/AboutMe.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Down from '../../public/down.svg'
-import Logo from '../../public/Logo.svg'
+import Face from '../../public/ccambium.jpg'
 import styles from './page.module.css'
 import Image from 'next/image'
 
@@ -13,14 +13,16 @@ export default function AboutMe() {
 			</div>
 			<div id={styles.myLife}>
 				<div id={styles.myLifeText}>
-					'LOREM IPSUM DOLOR'
+					<p>
+					Depuis que j'ai eu mon premier ordinateur, j'ai toujours été curieux de savoir comment fonctionnaient les programmes sur mon ordinateur, ou même comment construire un site web. Je me suis spécialisé dans ce domaine et maintenant j'ai une meilleure compréhension de la programmation et je cherche de nouveaux projets !
+					</p>
 				</div>
 				<div id={styles.myLifeImg}>
-					<Image src={Logo} alt="IMG" />
+					<Image src={Face} alt="IMG" />
 				</div>
 			</div>
 			<div className={`${styles.next} ${styles.aboutMeNext}`}>
-				<p>Skills and Projects</p>
+				<p>Skills & Projects</p>
 				<Image src={Down} alt="v" className={styles.nextDown} id={styles.nextAboutMe}/>
 			</div>
 		</main>
