0555db82374712d0a113bb4a486ab0ecb443a782
Camille CAMBIUM <royalfr@protonmail.com>
2024-03-16 15:33:45 +0100

    UPDATE : app/SkillsAndProjects.tsx app/page.module.css
    Adding arrows, changing text font, starting displaying of skill/projects

--- srcs/app/src/app/SkillsAndProjects.tsx
+++ srcs/app/src/app/SkillsAndProjects.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import Down from '../../public/down.svg'
 import styles from './page.module.css'
+import left from '../../public/left-arrow-svgrepo-com.svg'
 import Image from 'next/image'
 
 export default function SkillsAndProjects() {
@@ -10,7 +10,14 @@ export default function SkillsAndProjects() {
 			<div className={`${styles.title}`}>
 				<h1>Skills</h1>
 			</div>
+
 			<div className={styles.box} id={styles.skills}>
+				<div id={styles.left} className={styles.nextSP}>
+					<Image src={left} alt="<" />
+				</div>
+				<div className={styles.list} id={styles.skillsList}>
+					<p> I'm a skill </p>
+				</div>
 			</div>
 			
 			<div className={`${styles.title}`} id={styles.projectTitle}>
@@ -18,6 +25,13 @@ export default function SkillsAndProjects() {
 			</div>
 
 			<div className={styles.box} id={styles.projects}>
+				<div className={styles.list} id={styles.skillsList}>
+					<p> I'm a project </p>
+				</div>
+
+				<div id={styles.right} className={styles.nextSP} >
+					<Image src={left} alt='>' />
+				</div>
 			</div>
 
 		{/*<div className={`${styles.next}`}>
